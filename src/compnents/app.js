f066d322072238e28ecfe35f3117ee99b73a2833
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-17 01:08:46 -0500

    break up index into files and components

---
+++ src/compnents/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import List from "./list";
+function App() {
+  return (
+    <div>
+      <h1>My Favourite Foods</h1>
+      <List />
+    </div>
+  );
+}
+
+export default App;
